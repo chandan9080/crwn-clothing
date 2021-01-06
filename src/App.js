2e3e6e0e6c949f50f296053bf0a7591f03f8ddce
chandan9080 <54211830+chandan9080@users.noreply.github.com>
2021-01-06 11:32:42 +0530

    Header with logo contact and shop page

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import HomePage from './pages/hompage/hompage.componet'
-import { Route } from 'react-router-dom'
+import { Switch, Route } from 'react-router-dom'
 import ShopPage from './pages/shoppage/shoppage.component'
-
+import './App.css'
+import Header from './components/header/header.component'
 function App() {
   return (
     <div>
-      <Route exact path="/" component={HomePage} />
-      <Route path="/shop" component={ShopPage} />
+      <Header />
+      <Switch>
+        <Route exact path="/" component={HomePage} />
+        <Route path="/shop" component={ShopPage} />
+      </Switch>
     </div>
   )
 }
