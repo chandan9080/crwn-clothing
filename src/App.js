fcab513b024e3186fca0f1c1bfa31dbd60e2ad33
chandan9080 <54211830+chandan9080@users.noreply.github.com>
2021-01-08 15:32:17 +0530

    redirect feature added when the user is already register

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
 import HomePage from './pages/hompage/hompage.componet'
-import { Switch, Route } from 'react-router-dom'
+import { Switch, Route, Redirect } from 'react-router-dom'
 import ShopPage from './pages/shoppage/shoppage.component'
 import SignInAndSignUp from './pages/sign-in-and-sign-out/sign-in-and-sign-up'
 import './App.css'
@@ -21,7 +21,6 @@ class App extends Component {
             id: snapshot.id,
             ...snapshot.data(),
           })
-          console.log(this.state)
         })
       }
       setCurrentUser(userAuth)
@@ -38,13 +37,22 @@ class App extends Component {
         <Switch>
           <Route exact path="/" component={HomePage} />
           <Route path="/shop" component={ShopPage} />
-          <Route path="/sign" component={SignInAndSignUp} />
+          <Route
+            exact
+            path="/sign"
+            render={() =>
+              this.props.currentUser ? <Redirect to="/" /> : <SignInAndSignUp />
+            }
+          />
         </Switch>
       </div>
     )
   }
 }
+const mapStateToProps = ({ user }) => ({
+  currentUser: user.currentUser,
+})
 const matchDispatchToProps = (dispatch) => ({
   setCurrentUser: (user) => dispatch(setCurrentUser(user)),
 })
-export default connect(null, matchDispatchToProps)(App)
+export default connect(mapStateToProps, matchDispatchToProps)(App)
--- src/redux/reducer/users/user.reducer.jsx
+++ src/redux/reducer/users/user.reducer.jsx
@@ -1,9 +1,10 @@
+import { userActionType } from './user.types'
 const SET_INITIAL_STATE = {
     currentUser: null,
 }
 const userReducer = (state = SET_INITIAL_STATE, action) => {
     switch (action.type) {
-        case 'SET_CURRENT_USER':
+        case userActionType.SET_CURRENT_USER:
             return {
                 ...state,
                 currentUser: action.payload,
