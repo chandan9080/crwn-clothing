b552c2b0d13bcced5b3499630fad8f438e52e001
chandan9080 <54211830+chandan9080@users.noreply.github.com>
2021-01-25 15:32:27 +0530

    cart dropdown icon made

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -1,15 +1,22 @@
 import React from 'react'
 import CustomButton from '../custom-button/custom-button.component'
 import './cart-dropdown.style.scss'
+import { connect } from 'react-redux'
+import CartItem from '../cart-item/cart-item.component'
 
-const CartDropdowm = () => {
+const CartDropdowm = ({ cartItems }) => {
   return (
     <div className="cart-dropdown">
       <div className="cart-items">
+        {cartItems.map((cartItem) => (
+          <CartItem key={cartItems.id} item={cartItem}></CartItem>
+        ))}
         <CustomButton>Go To Checkout</CustomButton>
       </div>
     </div>
   )
 }
-
-export default CartDropdowm
+const mapStateToProps = ({ cart: { cartItems } }) => ({
+  cartItems,
+})
+export default connect(mapStateToProps, null)(CartDropdowm)
