63548bd347f2c81b25fca90b82b09361a30c7f74
chandan9080 <54211830+chandan9080@users.noreply.github.com>
2021-02-03 16:24:45 +0530

    redux actions are only visible in Development enviornment

--- src/components/preview-collection/collection-preview.component.jsx
+++ src/components/preview-collection/collection-preview.component.jsx
@@ -4,10 +4,10 @@ import CollectionItems from '../collection-items/collection-items.component'
 
 const CollectionPreview = ({ title, items }) => (
   <div className="collection-preview ">
-    <h1 className="title">{title.toUpperCase()}</h1>
+    <h1 className="title" >{title.toUpperCase()}</h1>
     <div className="preview">
       {items
-        .filter((item, idx) => idx < 4)
+        .filter((_item, idx) => idx < 4)
         .map((item) => (
           <CollectionItems key={item.id} item={item} />
         ))}
